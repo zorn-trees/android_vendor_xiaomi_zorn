e98dcef0542bb521f6b5ecdf050ed9587c96aca4
SMGReborn <vherawidatama@gmail.com>
2025-09-11 15:54:04 +0000

    zorn: initial decommonize
    Signed-off-by: SMGReborn <vherawidatama@gmail.com>

--- Android.mk
+++ Android.mk
@@ -3,3 +3,29 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),zorn)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,0b8aef1c95cb58f3b084be0570a1567d823a6318)
+$(call add-radio-file-sha1-checked,radio/aop.img,629bbf8bcfd26ee097f2bcc6b793a3e6da92e691)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,52d4eca0c4a6104dc1a5ad043556ab5204ebf285)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,f2750f76e8a64855d3122c3bd2f4a6486380e50e)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,e8263e59a58671d3035ca4da7c1c63d187e69db4)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,b8db7c575ca82b8a96a16521fa35ca778ec063bb)
+$(call add-radio-file-sha1-checked,radio/dsp.img,2c959f35bdd7de07b6284949478b027720b2e5c5)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,e07014fd968523e392bf9b98decce23d9e971d55)
+$(call add-radio-file-sha1-checked,radio/hyp.img,08e77426b643b0bb9e9d484fd2215ae7be622e1b)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,26481210170ce6d55d1b233267cb44c555cdeaa4)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,89a7a2629c7b122bacb6f73fd66ad89164d99d60)
+$(call add-radio-file-sha1-checked,radio/modem.img,da852c7e912ca0e23ea7adbf1f852b151a318be9)
+$(call add-radio-file-sha1-checked,radio/modemfirmware.img,0def57533d6276c1d363938301ca6c2cfa11a870)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,c2f40bda345537c1855f7819bdba9f4fb1251b28)
+$(call add-radio-file-sha1-checked,radio/shrm.img,152d4db32c11f00670c126ea8f56cb110f3b2944)
+$(call add-radio-file-sha1-checked,radio/tz.img,5095143d74d7dad907b4f15a5b8e703ebc092dcf)
+$(call add-radio-file-sha1-checked,radio/uefi.img,f64e880ed863d48a316cefae5fe010dea30a9e0c)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,318d5fe07eda1607f4126fc0d94d24946a51e84f)
+$(call add-radio-file-sha1-checked,radio/xbl.img,d7f6fd2a0b863ca1b0ccd3d52ff653bfc966eff3)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,b3bff26e15b96b6f40262042e3ce47cd4551c392)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,027e70db26b57ded0386fc70c6a6f4f18684d249)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,26 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    modemfirmware \
+    qupfw \
+    shrm \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
